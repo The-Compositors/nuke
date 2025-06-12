0c409f17e1234cbfa877781564620a6b835db65a
soluleaf <58694798+soluleaf@users.noreply.github.com>
2025-06-13 00:38:42 +0900

    Create reload_all.py

---
+++ reload_all.py
@@ -0,0 +1,17 @@
+import nuke 
+
+def reeload_selected():
+    nodes = nuke.selectedNode()
+    for node in nodes:
+        if node.Class() == "Read":
+            node.knob("reload").execute()
+
+
+def reload_all():
+    nodes = nuke.allNodes()
+    for node in nodes:
+        if node.Class() == "Read":
+            node.knob("reload").execute()
+
+
+nuke.menu("Nuke").findItem("Edit").addCommand("reload_all", 'reload_all()', "alt+r")
